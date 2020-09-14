d4a8f359a40a72619c2695c415aba856b02c5c83
root <root@ip-172-31-40-156.ec2.internal>
2020-09-14 19:01:50 +0000

    cambio de idioma

--- empleado/settings/base.py
+++ empleado/settings/base.py
@@ -84,7 +84,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'es-pe'
+LANGUAGE_CODE = 'es-mx'
 
 TIME_ZONE = 'UTC'
 
